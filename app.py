b3745108757c6360d5c22b0c3246d5e346010134
Kyle Green <70210492+greenkg24@users.noreply.github.com>
2021-11-24 08:37:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,35 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+
+db = SQLAlchemy(app)
+
+class City(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50), nullable=False)
+  
 
 @app.route('/')
 def index():
+  cities = City.query.all()
+  
   url = 'api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=b3cb766639dc5ea608f6017ede1df5f6'
-  city = 'Las Vegas'
   
-  r = requests.get(url.format(city)).json()
-  print(r)
+  weather_data = []
   
-  weather = {
-    'city': city,
-    'temperature': r['main']['temp'],
-    'description': r['weather'][0]['description'],
-    'icon': r['weather'][0]['icon'],
-  }
+  for city in cities:
+    
+    r = requests.get(url.format(city)).json()
+    print(r)
+
+    weather = {
+      'city': city,
+      'temperature': r['main']['temp'],
+      'description': r['weather'][0]['description'],
+      'icon': r['weather'][0]['icon'],
+    }
+    
+    weather_data.append(weather)
   
   return render_template('weather.html' weather=weather)
